bcd35b65a98b5937a489fe0e321ac385c33c61cf
Nikhil Kumar <nikhilkumar08112000@gmail.com>
2024-09-06 01:13:02 +0530

    This code is for evenodd

---
+++ .vscode/evenodd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cout << "enter the i";
+    cin >> i;
+    if (i % 2 == 0)
+    {
+        cout << "even" << endl;
+    }
+    else
+    {
+        cout << "odd" << endl;
+    }
+    return 0;
+}

f480606cc902b91c05d5679f7bc85538bfce58bf
Nikhil Kumar <nikhilkumar08112000@gmail.com>
2024-09-13 22:12:22 +0530

    This code is for stack

---
+++ .vscode/stackstl.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    stack<string> s;
+    s.push("Nikhil");
+    s.push("kumar");
+    s.push("singh");
+    cout << "Top element:" << s.top() << endl;
+}

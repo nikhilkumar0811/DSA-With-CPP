f76fa46784e1f229f62edf7eeb1bfb6f49dc70a4
Nikhil Kumar <nikhilkumar08112000@gmail.com>
2024-10-21 21:09:21 +0530

    table2to10    in  c++

---
+++ .vscode/table2to10.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+void table()
+{
+    for (int num = 2; num <= 10; num++)
+    {
+        cout << "table of" << num
+             << ".\n"
+             << endl;
+        for (int i = 1; i <= 10; i++)
+        {
+            cout << num << "x" << i << "=" << num * i << endl;
+        }
+    }
+}
+int main()
+{
+    table();
+    return 0;
+}

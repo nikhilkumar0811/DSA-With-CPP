711550e63d87d51b16ef8069462ace7914bf521d
Nikhil Kumar <nikhilkumar08112000@gmail.com>
2024-11-22 23:42:09 +0530

    sum of all number   c++

---
+++ sumofallnumber.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int sum = 0;
+    for (int i = 1; i <= 1500; i++)
+    {
+        sum += i;
+    }
+    cout << "THe sum of numbers from 1 to 1500 is:" << sum;
+    return 0;
+}

3ca380cc3b815039afdf3ebdc5627bcad054c0d8
Nikhil Kumar <nikhilkumar08112000@gmail.com>
2024-11-23 21:07:15 +0530

    cube of number   c++

---
+++ cubeofallnumber.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n = 10;
+
+    for (int i = 1; i <= n; i++)
+    {
+        int cube = i * i * i;
+        cout << "Cube of " << i << " is: " << cube << endl;
+    }
+
+    return 0;
+}

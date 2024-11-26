f3376ea75453bb4946ca9de5cd41b877b7bba0f8
Nikhil Kumar <nikhilkumar08112000@gmail.com>
2024-11-26 22:23:23 +0530

    lcm in   c++

--- 2Dbinarysearch.cpp
+++ 2Dbinarysearch.cpp
@@ -1,5 +1,52 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-bool binarysearch(vector<vector<int>>&matirx,int target){
-    
+bool binarysearch(vector<vector<int>> &matrix, int target)
+{
+    int rows = matrix.size();
+    if (rows == 0)
+        return false;
+    int cols = matrix[0].size();
+
+    int start = 0;
+    int end = rows * cols - 1;
+
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        int midvalue = matrix[mid / cols][mid % rows];
+
+        if (midvalue == target)
+        {
+            return true;
+        }
+        else if (midvalue < target)
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+    }
+    return false;
+}
+int main()
+{
+    int rows, cols, target;
+    cout << "enter the number of rows and colums:" << endl;
+    cin >> rows >> cols;
+
+    vector<vector<int>> matrix(rows, vector<int>(cols));
+    cout << "enter the target values to search:";
+    cin >> target;
+
+    if (binarysearch(matrix, target))
+    {
+        cout << "Target found in the matrix!" << endl;
+    }
+    else
+    {
+        cout << "Target not found in the matrix.";
+    }
+    return 0;
 }
